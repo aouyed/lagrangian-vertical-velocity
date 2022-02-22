f8cf2fa2eafc0182e11bd1c27cb1d7fa2031b8fa
Amir Ouyed <aouyed@Amirs-MacBook-Pro.local>
2022-02-22 15:08:14 -0700

    hello

--- src/cloud_tracker.py
+++ src/cloud_tracker.py
@@ -109,7 +109,6 @@ def object_tracker():
     ds=xr.open_dataset(file)
     ds_total, cmap =contour_loop(ds)  
     ds_total.to_netcdf('../data/processed/tracked.nc')
-    #m.plot_loop(ds_total, 'id_map', c.implot, 0, 1000,cmap,m.FOLDER)
    
 
     return ds_total
@@ -147,6 +146,23 @@ def time_series(ds_total, ids):
     ds_time_series=xr.Dataset.from_dataframe(df)
     
     return ds_time_series    
+
+
+def contour_averages(ds_total, ids):
+    data={'time':[], 'id':[],'size':[],'pressure_vel':[],'pressure_ten':[],'cloud_top_pressure':[]}
+
+    for idno in tqdm(ids[ids!=0]): 
+        ds=ds_total.where(ds_total.id_map==idno)
+        data=time_loop(data, ds, idno)
+            
+            
+        
+    df=pd.DataFrame(data)
+    df=df.set_index(['time','id'])
+    ds_time_series=xr.Dataset.from_dataframe(df)
+    
+    return ds_time_series    
+        
         
 
 def time_series_plotter(ds, label):
@@ -164,11 +180,11 @@ def time_series_plotter(ds, label):
 def scatter_plot(ds, label):
     df=ds.to_dataframe().reset_index()
     fig, ax= plt.subplots()
+    #ax.scatter(df[label],df['pressure_rate'])
     ax.scatter(df[label],df['pressure_vel'])
     ax.scatter(df[label],df['pressure_ten'])
-    
-    if label is 'size':
-        ax.set_xlim(0,200)
+    #ax.scatter(df[label],df['pressure_rate'])
+    ax.set_xlim(-10,10)
 
     plt.show()
     plt.close()
@@ -181,12 +197,7 @@ def scatter_general(ds, labelx, labely):
     plt.show()
     plt.close()
     
-    
-def main():
-    #ds_total=object_tracker()
-    ds_total=xr.open_dataset('../data/processed/tracked.nc')
-    print(ds_total)
-    
+def time_series():
     ds_total['size_map']=np.sqrt(ds_total.area_map)
     ds_total['pressure_vel']=100*ds_total['pressure_vel']
     ds_total['pressure_tendency']=100*ds_total['pressure_tendency']
@@ -194,21 +205,65 @@ def main():
 
     ds_time_series=time_series(ds_total, ids)
     ds_time_series.to_netcdf('../data/processed/time_series_complete_median.nc')
-    ds_time_series=xr.open_dataset('../data/processed/time_series_complete_median.nc')
-    ds_time_series=ds_time_series.rolling(time=3, center=True).mean()
-    ds_time_series['size_rate']=ds_time_series['size'].differentiate("time",datetime_unit='h' )
+   
 
-    #time_series_plotter(ds_time_series, 'pressure_vel')
-    #time_series_plotter(ds_time_series, 'size')
-    #time_series_plotter(ds_time_series, 'size_rate')
-    #time_series_plotter(ds_time_series, 'pressure_vel')
-    scatter_plot(ds_time_series, 'size_rate')
-    scatter_plot(ds_time_series, 'size')
-    scatter_plot(ds_time_series, 'cloud_top_pressure')
-    scatter_general(ds_time_series, 'size', 'size_rate')
+def cloud_plotter(da, vmin=-1, vmax=1, cmap='RdBu'):
+    if vmin!=vmax:
+        da.plot(vmin=-1, vmax=1, cmap=cmap)
+    else:
+        da.plot(cmap=cmap)
+    plt.show()
+    plt.close()
 
-    breakpoint()
 
+def mean_clouds(ds_time_series, ds_total):
+    ds_total['pressure_vel_mean']=  xr.full_like(ds['pressure_vel'], fill_value=np.nan)
+    ds_total['pressure_ten_mean']=  xr.full_like(ds['pressure_tendency'], fill_value=np.nan)
+    ds_total['pressure_mean']=  xr.full_like(ds['cloud_top_pressure'], fill_value=np.nan)
+    ds_total['pressure_rate']=  xr.full_like(ds['cloud_top_pressure'], fill_value=np.nan)
+    df=ds_total.to_dataframe()
+    for time in ds_time_series['time'].values:
+            for idno in ds_time_series['id'].values:
+                
+
+        
+        
+    
+    
+
+def analyzer(ds_time_series, ds_total):
+    
+    condition1=ds_time_series.pressure_adv< ds_time_series['pressure_adv'].quantile(0.95).item()
+    #condition2=abs(ds_time_series.size_rate)<
+    ds_sample=ds_time_series
+    scatter_plot(ds_sample, 'size_rate')
+
+    scatter_general(ds_sample, 'size_rate', 'pressure_vel')
+    scatter_general(ds_sample, 'size_rate', 'pressure_ten')
+    scatter_general(ds_sample, 'pressure_vel', 'pressure_rate')
+
+    df=ds_sample.to_dataframe().reset_index().dropna()
+    print(df[['id','size_rate','pressure_adv','size']])
+    ds=ds_total.sel(time=df['time'].values[11])
+    ds=ds.where(ds.id_map==df['id'].values[11])
+    #ds=ds.where(ds.lon >-90)
+    #ds['pressure_vel'].plot.hist()
+    #ds['cloud_top_pressure'].plot.hist()
+    df=ds.to_dataframe().reset_index().dropna().set_index(['time','lat','lon'])
+    print(df.reset_index())
+
+    ds_small=xr.Dataset().from_dataframe(df)
+    cloud_plotter(ds_small['pressure_vel'])
+    cloud_plotter(ds_small['pressure_tendency'])
+    cloud_plotter(ds_small['cloud_top_pressure'],cmap='viridis', vmin=0, vmax=0)
+    cloud_plotter(ds['id_map'],cmap='viridis', vmin=0, vmax=0)
+
+
+
+    print(ds)
+    
+    
+def animation(ds_total):
     m.plot_loop(ds_total, 'thresh_map', c.implot, 0, 255,'viridis',m.FOLDER)
     m.plot_loop(ds_total, 'id_map', c.implot, 0, 1000,cmap,m.FOLDER)
     m.plot_loop(ds_total, 'size_map', c.implot, 0, 100,'viridis',m.FOLDER)
@@ -216,7 +271,38 @@ def main():
     m.plot_loop(ds_total, 'pressure_tendency', c.implot, -2, 2,'RdBu',m.FOLDER)
     m.plot_loop(ds_total, 'cloud_top_pressure', c.implot, 0, 1000,'viridis',m.FOLDER)
 
-    print(ds_total)
+    
+
+def main():
+    
+    #ds_total=object_tracker()
+
+    ds_total=xr.open_dataset('../data/processed/tracked.nc')
+    ds_total['size_map']=np.sqrt(ds_total.area_map)
+    ds_total['pressure_vel']=100*ds_total['pressure_vel']
+    ds_total['pressure_tendency']=100*ds_total['pressure_tendency']
+    ds_total['pressure_adv']= ds_total['pressure_vel']- ds_total['pressure_tendency']
+
+
+    ds_time_series=xr.open_dataset('../data/processed/time_series_complete.nc')
+    ds_time_series=ds_time_series.rolling(time=3, center=True).mean()
+    ds_time_series['size_rate']=ds_time_series['size'].differentiate("time",datetime_unit='h' )
+    ds_time_series['pressure_rate']=100*ds_time_series['cloud_top_pressure'].differentiate("time",datetime_unit='s' )
+    ds_time_series=ds_time_series.where(ds_time_series.size_rate)
+    ds_time_series['pressure_adv']=ds_time_series['pressure_vel'] -ds_time_series['pressure_ten']
+
+    #time_series_plotter(ds_time_series, 'pressure_vel')
+    #time_series_plotter(ds_time_series, 'size')
+    #time_series_plotter(ds_time_series, 'size_rate')
+    #time_series_plotter(ds_time_series, 'pressure_vel')
+    scatter_plot(ds_time_series, 'size_rate')
+    analyzer(ds_time_series, ds_total)
+    #scatter_plot(ds_time_series, 'size')
+    #scatter_plot(ds_time_series, 'cloud_top_pressure')
+    
+
+
+   
     
 if __name__ == "__main__":
     main()
